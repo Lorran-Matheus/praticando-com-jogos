1ab6f679e2cfc3aeb044b3782eb26c92a3cdb1ce
Lorran-Matheus <lorranmatheus940@gmail.com>
2025-06-19 12:21:53 -0300

    atualizações e arquivos

---
+++ Yu-Gi-Oh Jo-Ken-Po/reame.md
@@ -0,0 +1,8 @@
+# *Jogo de pedra papel tesoura*
+> Uma forma diferente...
+
+## Resumo
+
+- A ideia é fazer um jogo de pedra papel tesoura dinâmico.
+- Utilizando frameworks CSS para dar um tema de "Yu-Gi-Oh!" ao jogo.
+- Foco maior em CSS e JavaScript
---
+++ Yu-Gi-Oh Jo-Ken-Po/src/scripts/engine.js
@@ -0,0 +1,78 @@
+const state = {
+    score: {
+        playerScore: 0,
+        computerScore: 0,
+        scoreBox: document.getElementById("score_points"),
+    },
+    cardsSprites: {
+        avatar: document.getElementById("card-image"),
+        name: document.getElementById("card-name"),
+        type: document.getElementById("card-type"),
+    },
+    fieldCards: {
+        player: document.getElementById("player-field-card"),
+        computer: document.getElementById("computer-field-card")
+    },
+    actions: {
+        button: document.getElementById("next-duel"),
+    }
+}
+
+const playersSides = {
+    player1: "player-field-card",
+    player2: "computer-field-card",
+}
+
+const pathImages = "/src/assets/icons/"
+
+const cardData = [
+    {
+        id: 0,
+        name: "Blue Eyes White Dragon",
+        type: "Paper",
+        img: `${pathImages}dragon.png`,
+        WinOf: [1],
+        LoseOf: [2],
+    },
+    {
+        id: 1,
+        name: "Dark Magician",
+        type: "Rock",
+        img: `${pathImages}magician.png`,
+        WinOf: [2],
+        LoseOf: [0],
+    },
+    {
+        id: 2,
+        name: "Exodia",
+        type: "Scissors",
+        img: `${pathImages}exodia.png`,
+        WinOf: [0],
+        LoseOf: [1],
+    },
+];
+
+async function getRandomCardId() {
+    const randomIndex = Math.floor(Math.random() * cardData.length)
+    return cardData[randomIndex].id;
+}
+
+async function createCardImg(randomIdCard, fieldSide) {
+    const cardImage =document.createElement("img");
+    cardImage.setAttribute("height", "100px");
+}
+async function drawCards(cardNumbers, fieldSide) {
+    for (let i = 0; i < cardNumbers; i++) {
+        const randomIdCard = await getRandomCardId();
+        const cardImage = await createCardImg(randomIdCard, fieldSide);
+
+        document.getElementById(fieldSide).appendChild(cardImage);
+    }
+}
+
+function init() {
+    drawCards(5, playersSides.player1)
+    drawCards(5, playersSides.player2)
+}
+
+init();
